555af6504bc75323b2253832b85f08a01ddceed6
naruse666 <0x706f6b6f@gmail.com>
2024-12-27 20:34:09 +0900

    add mutating,validating webhook

--- chapter-7/internal/webhook/v1alpha/foo_webhook.go
+++ chapter-7/internal/webhook/v1alpha/foo_webhook.go
@@ -46,7 +46,7 @@ func SetupFooWebhookWithManager(mgr ctrl.Manager) error {
 
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
-// +kubebuilder:webhook:path=/mutate-samplecontroller-io-io-v1alpha-foo,mutating=true,failurePolicy=fail,sideEffects=None,groups=samplecontroller.io.io,resources=foos,verbs=create;update,versions=v1alpha,name=mfoo-v1alpha.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/mutate-samplecontroller-io-v1alpha-foo,mutating=true,failurePolicy=fail,sideEffects=None,groups=samplecontroller.io,resources=foos,verbs=create;update,versions=v1alpha,name=mfoo-v1alpha.kb.io,admissionReviewVersions=v1
 
 // FooCustomDefaulter struct is responsible for setting default values on the custom resource of the
 // Kind Foo when those are created or updated.
@@ -79,7 +79,7 @@ func (d *FooCustomDefaulter) Default(ctx context.Context, obj runtime.Object) er
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-// +kubebuilder:webhook:path=/validate-samplecontroller-io-io-v1alpha-foo,mutating=false,failurePolicy=fail,sideEffects=None,groups=samplecontroller.io.io,resources=foos,verbs=create;update,versions=v1alpha,name=vfoo-v1alpha.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-samplecontroller-io-v1alpha-foo,mutating=false,failurePolicy=fail,sideEffects=None,groups=samplecontroller.io,resources=foos,verbs=create;update,versions=v1alpha,name=vfoo-v1alpha.kb.io,admissionReviewVersions=v1
 
 // FooCustomValidator struct is responsible for validating the Foo resource
 // when it is created, updated, or deleted.
@@ -147,6 +147,6 @@ func validateFoo(foo *samplecontrolleriov1alpha.Foo) error {
 	}
 
 	return apierrors.NewInvalid(
-		schema.GroupKind{Group: "samplecontroller.io", Kind: "Foo"},
+		schema.GroupKind{Group: "samplecontroller", Kind: "Foo"},
 		foo.Name, allErrs)
 }
